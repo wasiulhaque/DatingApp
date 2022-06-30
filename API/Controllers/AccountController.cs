9274c670e5734e400b31c293c944d485411cdafb
wasiulhaque <wasiulhq@gmail.com>
2022-06-30 16:59:05 +0600

    Authentication Controllers and DTOs added

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseAPIController
+    {
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+     [HttpPost("register")]
+     public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
+     {
+        if(await UserExists(registerDTO.Username)) return BadRequest("Username is taken.");
+
+        using var hmac = new  HMACSHA512();
+        var user = new AppUser
+        {
+            UserName = registerDTO.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return user;
+     }
+
+     [HttpPost("login")]
+     public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+     {
+        var user = await _context.Users
+        .SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
+        
+        if(user == null) return Unauthorized("Invalid Username.");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+        for(int i = 0; i < computedHash.Length; i++)
+        {
+            if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Passowrd.");
+        }
+
+        return user;
+     }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+
+    }
+}
